dca13f925d861f284ce8e74960284aae48951093
bigcoso <abbase101@gmail.com>
2015-07-27 19:49:12 +0200

    Primer programa Java
    test

---
+++ src/first/holamundo/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package first.holamundo;
+
+/**
+ * @author war2u
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Vacas");
+	}
+
+}
